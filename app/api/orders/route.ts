f0234b3a75a7b9e3110dd35c3df2855fab8a2021
Shariq <shariqkhan4414@gmail.com>
2026-05-14 10:01:21 +0530

    updated

--- app/api/orders/route.ts
+++ app/api/orders/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-import { connectDB } from "@/lib/mongodb";
+import connectDB from "@/lib/mongodb";
 import Order from "@/models/Order";
 
 // =========================
